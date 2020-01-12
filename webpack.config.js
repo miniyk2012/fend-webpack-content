bdd35f6e908755a464d2e4247367d847bbf891b6
Thomas Young <yk_ecust_2007@163.com>
2020-01-12 21:40:35 +0800

    plugin, server 指定dist作为静态目录, 这样html就只能用这个静态目录里面的文件

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,22 @@
 const path = require('path');
 const webpack = require('webpack');
+const HtmlWebPackPlugin = require("html-webpack-plugin");
 
 module.exports = {
     entry: './src/client/index.js',
+    module: {
+        rules: [  //  配置loader
+            {
+                test: /\.js$/, //正则匹配命中要使用loader的文件
+                exclude: /node_modules/,
+                loader: "babel-loader"
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebPackPlugin({
+            template: "./src/client/views/index.html",
+            filename: "./index.html",
+        })
+    ]
 }
