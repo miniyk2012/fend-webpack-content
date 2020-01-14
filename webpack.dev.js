72439c1b75051b008b23ea37b27b737c5e33ec17
Thomas Young <yk_ecust_2007@163.com>
2020-01-14 09:26:33 +0800

    CleanWebpackPlugin

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const webpack = require('webpack');
 const HtmlWebPackPlugin = require("html-webpack-plugin");
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 
 module.exports = {
@@ -32,6 +32,14 @@ module.exports = {
             template: "./src/client/views/index.html", //模板路径
             filename: "./index.html", // //通过模板生成的文件名
         }),
-        
+        new CleanWebpackPlugin({
+            // Simulate the removal of files
+            dry: true,
+            // Write Logs to Console
+            verbose: true,
+            // Automatically remove all unused webpack assets on rebuild
+            cleanStaleWebpackAssets: true,
+            protectWebpackAssets: false
+        })
     ]
 }
